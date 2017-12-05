0f1771e44ab9dd1c547d1089a714a490f1e8f3f0
Glitch (timestamp-microserver1) <none>
2017-12-05 18:33:41 +0000

    🚜🐙 Checkpoint server.js:572355

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-var express = require('express')
+//This file is required for node
+
+var express = require('express');
 var app = express();
+
+app.get('/timestamp', function(req, res) {
+  res.send("timestamp");
+})
+
+
+app.listen(process.env.PORT, function () {
+  console.log('Node.js listening ...');
+});
 
